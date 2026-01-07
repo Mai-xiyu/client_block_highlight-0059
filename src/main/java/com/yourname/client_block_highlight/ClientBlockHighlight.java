0bc22c1c65b88ba573ce6689e182a3f39a3fccfe
Mai-xiyu <111494660+Mai-xiyu@users.noreply.github.com>
2026-01-07 22:05:54 +0800

    Add files via upload

--- src/main/java/com/yourname/client_block_highlight/ClientBlockHighlight.java
+++ src/main/java/com/yourname/client_block_highlight/ClientBlockHighlight.java
@@ -3,7 +3,9 @@ package com.yourname.client_block_highlight;
 import com.mojang.logging.LogUtils;
 import com.yourname.client_block_highlight.config.ClientConfig;
 import com.yourname.client_block_highlight.gui.ClientBlockHighlightConfigScreen;
+import com.yourname.client_block_highlight.util.KeyBindings;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent; // 导入
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -19,14 +21,23 @@ public class ClientBlockHighlight {
 
     public ClientBlockHighlight() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
+
         // Register client configuration
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ClientConfig.CLIENT_SPEC);
-        
+
         // Register custom config screen factory for the Mods list GUI
-        ModLoadingContext.get().registerExtension(ConfigScreenHandler.class, () -> new ConfigScreenHandler((minecraft, parent) -> new ClientBlockHighlightConfigScreen(parent)));
-        
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new ClientBlockHighlightConfigScreen(screen)));
+
+        // 注册按键绑定
+        modEventBus.addListener(this::registerBindings);
+
         // We register client event handlers via @Mod.EventBusSubscriber in renderer/manager classes
         MinecraftForge.EVENT_BUS.register(this);
     }
-} 
+
+    // 新增：注册按键的方法
+    private void registerBindings(RegisterKeyMappingsEvent event) {
+        event.register(KeyBindings.OPEN_CONFIG_KEY);
+    }
+}
